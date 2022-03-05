4a74942caa05510f85e3e7dfcbff8a7471f3a562
Nermin Topalovic <nerminto@edu.aau.at>
2022-03-05 18:22:20 +0100

    Connect the app with the server (not working yet)

---
+++ app/src/main/java/com/example/se2einzelphase/MainFragment.java
@@ -0,0 +1,79 @@
+package com.example.se2einzelphase;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.se2einzelphase.databinding.FragmentFirstBinding;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+
+public class MainFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+    private EditText matrikelnummerInput;
+    private Button oKButton;
+    private TextView serverAntwortField;
+    private TextView convertedMatrikelnummerField;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        oKButton = binding.OKButton;
+        serverAntwortField = binding.Serverantwort;
+        convertedMatrikelnummerField = binding.MatrikelnummerConverted;
+        matrikelnummerInput = binding.MatrikelnummerEingabe;
+        oKButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                try{
+                    Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+                    DataOutputStream outputStream = new DataOutputStream(clientSocket.getOutputStream());
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    outputStream.writeBytes(matrikelnummerInput.getText().toString());
+                    serverAntwortField.setText(bufferedReader.readLine());
+                    clientSocket.close();
+                }catch (IOException e){
+
+                }
+
+
+
+            }
+        });
+
+        return binding.getRoot();
+
+    }
+
+
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
